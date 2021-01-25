13bfaf12b9fa8f64c88c0c65731c8fe98290d7b7
unknown <michaelsorensenatwork@gmail.com>
2021-01-24 21:35:11 -0800

    final commit before packaging

--- app/src/assets/index.ts
+++ app/src/assets/index.ts
@@ -6,6 +6,7 @@ import { MDCTextField } from "@material/textfield";
 import { MDCTopAppBar } from "@material/top-app-bar";
 
 import "./clock";
+import { MDCLinearProgress } from "@material/linear-progress";
 
 const configFile = readFileSync(resolve(process.cwd(), "config.json"), "utf8");
 
@@ -26,7 +27,7 @@ scheduleFileManager.onChange((schedule) => {
 function renderSchedule(schedule: Schedule) {
   if (!scheduleContainer) return;
   scheduleContainer.innerHTML = "";
-  schedule.forEach((task) => {
+  schedule.forEach((task, i) => {
     if (task.interval === "startup") return;
     const taskElem = document.createElement("div");
     taskElem.classList.add("mdc-card");
@@ -34,10 +35,19 @@ function renderSchedule(schedule: Schedule) {
     console.log(task.lastExecuted + task.interval);
     taskElem.innerHTML = `
           <div class="task">
+            <div class="taskDetails">
               <p>${task.name}</p>
               <p>${task.commandPath}</p>
               <p>${scheduled.toLocaleString()}</p>
             </div>
+            <div class="mdc-touch-target-wrapper">
+              <button class="mdc-fab mdc-fab--mini mdc-fab--touch" onclick="deleteTask(${i})">
+                <div class="mdc-fab__ripple"></div>
+                <span class="material-icons mdc-fab__icon">delete</span>
+                <div class="mdc-fab__touch"></div>
+              </button>
+            </div>
+          </div>
           `;
     scheduleContainer.append(taskElem);
   });
@@ -76,4 +86,13 @@ if (form) {
     data.interval = parseInt(data.interval);
     scheduleFileManager.addTask(data);
   });
+}
+
+(window as any).deleteTask = (index: number) => {
+  scheduleFileManager.deleteTask(index);
+};
+
+const progressBarElem = document.querySelector("loader");
+if (progressBarElem) {
+  const linearProgress = new MDCLinearProgress(progressBarElem);
 }
