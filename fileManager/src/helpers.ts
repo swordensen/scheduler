8ec7e749cc3f8cb13c6ac64d2b1e3e05a575d62a
unknown <michaelsorensenatwork@gmail.com>
2021-03-01 19:11:05 -0800

    removing open from file manager

--- fileManager/src/helpers.ts
+++ fileManager/src/helpers.ts
@@ -1,19 +1,8 @@
 import { existsSync, mkdirSync } from "fs";
-import { resolve } from "path";
-import { logFolder } from "./defaults";
-import { Task } from "./types";
 
 export function createFolderIfNotExist(folderPath: string) {
   if (!existsSync(folderPath)) {
     mkdirSync(folderPath);
   }
   return folderPath;
-}
-
-/**
- * this function opens up the log file for viewing
- */
-export function openLogFile(task: Task) {
-  const path = resolve(logFolder, `./commands/${task.name}.log`);
-  if (existsSync(path)) open(path);
 }
