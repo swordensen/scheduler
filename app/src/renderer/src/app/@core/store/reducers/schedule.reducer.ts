28e52cc2aa558778a703710962dbc8fc4d5d8eeb
unknown <michaelsorensenatwork@gmail.com>
2021-04-25 14:30:22 -0700

    bumping version number

--- app/src/renderer/src/app/@core/store/reducers/schedule.reducer.ts
+++ app/src/renderer/src/app/@core/store/reducers/schedule.reducer.ts
@@ -1,12 +1,51 @@
 import { Action, createReducer, on } from '@ngrx/store';
-import { setSchedule } from '../actions/schedule.actions';
+import {
+  setSchedule,
+  taskAdded,
+  taskDeleted,
+  taskFailed,
+  taskStarted,
+  taskUpdated,
+  taskWaiting,
+} from '../actions/schedule.actions';
 import { Schedule } from '../../../../../../main/types';
 
 const initialScheduleState: Schedule = [];
 
 const _scheduleReducer = createReducer(
   initialScheduleState,
-  on(setSchedule, (state, { schedule }) => schedule)
+  on(setSchedule, (state, { schedule }) => schedule),
+  on(taskStarted, (state, { task }) =>
+    state.map((_task) => {
+      if (_task.id === task.id) return task;
+      return _task;
+    })
+  ),
+  on(taskWaiting, (state, { task }) =>
+    state.map((_task) => {
+      if (_task.id === task.id) return task;
+      return _task;
+    })
+  ),
+  on(taskFailed, (state, { task }) =>
+    state.map((_task) => {
+      if (_task.id === task.id) return task;
+      return _task;
+    })
+  ),
+  on(taskAdded, (state, { task }) => [...state, task]),
+  on(taskUpdated, (state, { task }) =>
+    state.map((_task) => {
+      if (_task.id === task.id) return task;
+      return _task;
+    })
+  ),
+  on(taskDeleted, (state, { task }) =>
+    state.reduce((accumulator, _task) => {
+      if (_task.id !== task.id) accumulator.push(_task);
+      return accumulator;
+    }, [] as Schedule)
+  )
 );
 
 export function scheduleReducer(state: Schedule | undefined, action: Action) {
