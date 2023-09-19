2e5eab889ef2cb9fe804b16248acdd6cbcf8bcf4
Michael Sorensen <msorensen@xifin.com>
2023-09-19 14:38:44 -0700

    grouping updates and side nav size adjuster thing

--- app/src/renderer/src/app/scaffolding/main/main.component.ts
+++ app/src/renderer/src/app/scaffolding/main/main.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { GUIState } from 'src/app/@core/store/reducers/gui.reducer';
 import { selectSideNavOpen } from 'src/app/@core/store/selectors/gui.selectors';
-
+import { ResizeEvent } from 'angular-resizable-element/public-api';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,5 +11,11 @@ import { selectSideNavOpen } from 'src/app/@core/store/selectors/gui.selectors';
 })
 export class MainComponent {
   sideNavOpened$ = this.store.select(selectSideNavOpen);
+  sideNavWidth = "300px";
   constructor(private store: Store<{ gui: GUIState }>) {}
+
+  handleSideNavResize(event:ResizeEvent){
+    console.log(event);
+    this.sideNavWidth = event.rectangle.width + "px";
+  }
 }
